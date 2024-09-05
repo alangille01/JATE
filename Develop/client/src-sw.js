d55f2670f64281127bb117d42f6e4892cd49d9f4
Alexis Langille <langille.alexis@gmail.com>
2024-09-04 23:40:55 -0400

    Revert "Temp Commit"
    This reverts commit e7f65dd20f690baf4c2cb7ef6816ef11e0e3d4f9.

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -1,35 +1,30 @@
-// Import the Workbox libraries
-import { precacheAndRoute } from 'workbox-precaching';
-import { registerRoute } from 'workbox-routing';
-import { StaleWhileRevalidate } from 'workbox-strategies';
+const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
+const { CacheFirst } = require('workbox-strategies');
+const { registerRoute } = require('workbox-routing');
+const { CacheableResponsePlugin } = require('workbox-cacheable-response');
+const { ExpirationPlugin } = require('workbox-expiration');
+const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
-// Precache the assets
 precacheAndRoute(self.__WB_MANIFEST);
 
-// Register route for caching requests to the index.html page for offline support
-registerRoute(
-  ({ request }) => request.mode === 'navigate',
-  new StaleWhileRevalidate({
-    cacheName: 'page-cache',
-  })
-);
-
-// Register route for caching other assets like JS and CSS files
-registerRoute(
-  ({ request }) =>
-    request.destination === 'style' ||
-    request.destination === 'script' ||
-    request.destination === 'worker',
-  new StaleWhileRevalidate({
-    cacheName: 'asset-cache',
-  })
-);
-
-// Listen to the install event and activate the new service worker immediately
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
+const pageCache = new CacheFirst({
+  cacheName: 'page-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
+warmStrategyCache({
+  urls: ['/index.html', '/'],
+  strategy: pageCache,
 });
+
+registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+
+// TODO: Implement asset caching
+registerRoute();
--- Develop/client/src/js/editor.js
+++ Develop/client/src/js/editor.js
@@ -1,36 +1,42 @@
+// Import methods to save and get data from the indexedDB database in './database.js'
 import { getDb, putDb } from './database';
 import { header } from './header';
 
-// Initialize CodeMirror editor
-const loadEditor = () => {
-  const editor = CodeMirror(document.querySelector('#main'), {
-    value: '',
-    mode: 'javascript',
-    theme: 'monokai',
-    lineNumbers: true,
-    lineWrapping: true,
-    autofocus: true,
-    indentUnit: 2,
-    tabSize: 2,
-  });
+export default class {
+  constructor() {
+    const localData = localStorage.getItem('content');
 
-  // Retrieve content from IndexedDB and set it in the editor
-  getDb().then((data) => {
-    console.info('Loaded data from IndexedDB, inserting into editor');
-    editor.setValue(data || header);
-  });
+    // check if CodeMirror is loaded
+    if (typeof CodeMirror === 'undefined') {
+      throw new Error('CodeMirror is not loaded');
+    }
 
-  // Save the content in IndexedDB on each change
-  editor.on('change', () => {
-    console.log('Editor content changed, saving to IndexedDB');
-    putDb(editor.getValue());
-  });
-};
+    this.editor = CodeMirror(document.querySelector('#main'), {
+      value: '',
+      mode: 'javascript',
+      theme: 'monokai',
+      lineNumbers: true,
+      lineWrapping: true,
+      autofocus: true,
+      indentUnit: 2,
+      tabSize: 2,
+    });
 
-// If the DOM is fully loaded, load the editor
-window.addEventListener('DOMContentLoaded', () => {
-  loadEditor();
-});
+    // When the editor is ready, set the value to whatever is stored in indexeddb.
+    // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
+    getDb().then((data) => {
+      console.info('Loaded data from IndexedDB, injecting into editor');
+      this.editor.setValue(data || localData || header);
+    });
 
-// Export the loadEditor function as the default export
-export default loadEditor;
+    this.editor.on('change', () => {
+      localStorage.setItem('content', this.editor.getValue());
+    });
+
+    // Save the content of the editor when the editor itself is loses focus
+    this.editor.on('blur', () => {
+      console.log('The editor has lost focus');
+      putDb(localStorage.getItem('content'));
+    });
+  }
+}
